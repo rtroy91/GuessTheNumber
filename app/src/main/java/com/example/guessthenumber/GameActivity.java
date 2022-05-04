af86925b8fa05dfdc3f3a82959e7320f9469d5d0
rtroy91 <rtbocasiones@bpsu.edu.ph>
2022-05-04 12:24:35 +0800

    1.2

--- app/src/main/java/com/example/guessthenumber/GameActivity.java
+++ app/src/main/java/com/example/guessthenumber/GameActivity.java
@@ -2,9 +2,14 @@ package com.example.guessthenumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,23 +19,57 @@ import java.util.Random;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public static final int MAX_NUMBER = 1000;
-    public static final Random RANDOM = new Random();
-    private TextView tvHint;
+    final Random random = new Random();
+
+    private TextView tvHint, tvName, tvInt, tvMax, tvDisplayDiff;
     private EditText etInput;
     private Button btnGuess;
     private int numberToFind, numberTries;
+    private ImageView ivZhongli;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         tvHint = (TextView) findViewById(R.id.tvHint);
+        tvDisplayDiff = (TextView) findViewById(R.id.tvDisplayDiff);
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvMax = (TextView) findViewById(R.id.tvMax);
+        tvInt = (TextView) findViewById(R.id.tvInt);
         etInput = (EditText) findViewById(R.id.etInput);
         btnGuess = (Button) findViewById(R.id.btnGuess);
         btnGuess.setOnClickListener(this);
 
-        newGame();
+        Intent intent = getIntent();
+        String name, gender;
+        name = intent.getStringExtra("NAME");
+        gender = intent.getStringExtra("GENDER");
+
+        if (gender.equalsIgnoreCase("Male")) {
+            tvName.setText("WELCOME! Mr. " + name);
+        } else if (gender.equalsIgnoreCase("Female")) {
+            tvName.setText("WELCOME! Ms. " + name);
+        }
+
+        String str = intent.getStringExtra("DIFFICULTY");
+        tvMax.setText(str);
+        int MAX_DIFF = Integer.parseInt(tvMax.getText().toString());
+        newGame(MAX_DIFF);
+        tvInt.setText(Integer.toString(numberToFind));
+
+        if(MAX_DIFF == 50){
+            tvDisplayDiff.setText("EASY");
+            tvDisplayDiff.setTextColor(Color.parseColor("#2980B9"));
+        }else if(MAX_DIFF == 250){
+            tvDisplayDiff.setText("NORMAL");
+            tvDisplayDiff.setTextColor(Color.parseColor("#229954"));
+        }else if(MAX_DIFF == 500){
+            tvDisplayDiff.setText("HARD");
+            tvDisplayDiff.setTextColor(Color.parseColor("#D35400"));
+        }else{
+            tvDisplayDiff.setText("EXTREME");
+            tvDisplayDiff.setTextColor(Color.parseColor("#B22727"));
+        }
     }
     @Override
     public void onClick(View view) {
@@ -42,21 +81,40 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     private void validate() {
         int n = Integer.parseInt(etInput.getText().toString());
         numberTries++;
+        ivZhongli = (ImageView) findViewById(R.id.ivZhongli);
+        final Dialog dialog = new Dialog(GameActivity.this);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                dialog.dismiss();
+            }
+        }, 2000);
 
         if (n == numberToFind) {
             Toast.makeText(this, "Congratulations ! You found the number " + numberToFind +
                     " in " + numberTries + " tries", Toast.LENGTH_SHORT).show();
-            newGame();
+            ivZhongli.setVisibility(View.INVISIBLE);
+            int MAX_DIFF = Integer.parseInt(tvMax.getText().toString());
+            newGame(MAX_DIFF);
+
+
         } else if (n > numberToFind) {
             tvHint.setText("TOO HIGH");
+            dialog.setContentView(R.layout.dialog_toohigh);
+            dialog.show();
+
         } else if (n < numberToFind) {
             tvHint.setText("TOO LOW");
+            dialog.setContentView(R.layout.dialog_toolow);
+            dialog.show();
         }
     }
 
-    private void newGame() {
-        numberToFind = RANDOM.nextInt(MAX_NUMBER) + 1;
-        tvHint.setText("START");
+    private void newGame(int DIFF) {
+        int MAX_NUMBER = random.nextInt(DIFF);
+        numberToFind = random.nextInt(MAX_NUMBER);
+        tvHint.setText("?");
         etInput.setText("");
         numberTries = 0;
     }
--- app/src/main/java/com/example/guessthenumber/MainActivity.java
+++ app/src/main/java/com/example/guessthenumber/MainActivity.java
@@ -1,44 +1,111 @@
 package com.example.guessthenumber;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner spDifficulty;
+    EditText etName, etGender;
+    TextView tvDiffVal, tvAlert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        etName = (EditText) findViewById(R.id.etName);
+        etGender = (EditText) findViewById(R.id.etGender);
         spDifficulty = (Spinner) findViewById(R.id.spDifficulty);
+
         ArrayList<String> data = new ArrayList<String>();
-        data.add("Select Gender");
-        data.add("Male");
-        data.add("Female");
+        data.add("Choose Difficulty");
+        data.add("Easy (MAX: 50)");
+        data.add("Normal (MAX: 250)");
+        data.add("Hard (MAX: 500)");
+        data.add("Extreme (MAX: 1000)");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, data);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spDifficulty.setAdapter(adapter);
 
+        tvDiffVal = (TextView) findViewById(R.id.tvDiffVal);
         final Button btnStart = findViewById(R.id.btnStart);
-        
+
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent start = new Intent(MainActivity.this, GameActivity.class);
-                startActivity(start);
+
+                String name, gender, mode, val, i;
+                name = etName.getText().toString();
+                gender = etGender.getText().toString();
+
+                start.putExtra("NAME", name);
+                start.putExtra("GENDER", gender);
+
+                val = spDifficulty.getSelectedItem().toString();
+                i = val.replaceAll("[^0-9]", "");
+                tvDiffVal.setText(i);
+
+                mode = tvDiffVal.getText().toString();
+                start.putExtra("DIFFICULTY", mode);
+
+                if(name.equals("")){
+                    Toast toast = Toast.makeText(MainActivity.this, "Please Type Your Name", Toast.LENGTH_SHORT);
+                    toast.show();
+                }else if(gender.equals("")){
+                    Toast toast = Toast.makeText(MainActivity.this, "Please Type Your Gender", Toast.LENGTH_SHORT);
+                    toast.show();
+                }else if(gender.compareToIgnoreCase("Male") > 0 && gender.compareToIgnoreCase("Female") > 0){
+                    Toast toast = Toast.makeText(MainActivity.this, "Wrong Gender Input", Toast.LENGTH_SHORT);
+                    toast.show();
+                }else if (mode.equals("")) {
+                    Toast toast = Toast.makeText(MainActivity.this, "Please Choose Difficulty", Toast.LENGTH_SHORT);
+                    toast.show();
+                } else {
+                    startActivity(start);
+                }
+            }
+        });
+
+        tvAlert = (TextView) findViewById(R.id.tvAlert);
+        final Button btnExit = findViewById(R.id.btnExit);
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setCancelable(true);
+                builder.setMessage("Are you sure you want to exit?");
+
+                builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                builder.show();
             }
         });
     }
